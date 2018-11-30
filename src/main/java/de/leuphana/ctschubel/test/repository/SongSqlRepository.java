7e76d64d48312393591c5c0ec2c39d493ee5c471
chris <ctschubel@googlemail.com>
2018-11-30 11:13:09 +0100

    Change from java 1.8 to java 11; add javadoc

--- src/main/java/de/leuphana/ctschubel/test/repository/SongSqlRepository.java
+++ src/main/java/de/leuphana/ctschubel/test/repository/SongSqlRepository.java
@@ -1,9 +1,14 @@
 package de.leuphana.ctschubel.test.repository;
 
-import de.leuphana.ctschubel.test.model.Song;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import de.leuphana.ctschubel.test.model.Song;
+
+/**
+ * SQL crud repository for {@link Song} objects
+ * @author ctschubel
+ */
 @Repository
 public interface SongSqlRepository extends CrudRepository <Song, Integer>{
 
